1d9a4d12aaa2537741f69b833e8295bb21126cc5
Colin <kappa6708@gmail.com>
2025-06-28 10:04:45 -0500

    finished unit 5

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
 from lists.models import Item
 
 class HomePageTest(TestCase):
@@ -19,11 +17,24 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<input name="item_text"')
         self.assertContains(response, '<input name="item_text'"")
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
+
     def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"})
+        response = self.client.post("/", data={"item_text": "A new list item"})
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
+
+        self.assertRedirects(response, "/")
 
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,10 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == "POST":
-        item = Item()
-        item.text = request.POST["item_text"]
-        item.save()
+        Item.objects.create(text=request.POST["item_text"])
         return redirect("/")
 
-    return render(
-        request,
-        "home.html",
-        {"new_item_text": request.POST.get("item_text", "")},
-    )
+    items = Item.objects.all()
+    return render(request, "home.html", {"items": items})
+
+
